eb7340fc73025c62dd513f48d90d6d5dea1d1524
infinitycat <zouliwei4848@outlook.com>
2025-04-06 01:44:22 +0800

    test tls

--- nonebot_plugin_maibot_adapters/bot.py
+++ nonebot_plugin_maibot_adapters/bot.py
@@ -26,13 +26,11 @@ config = Config()
 # 定义日志配置
 
 class ChatBot:
-    context = ssl.create_default_context()
-    context.maximum_version = ssl.TLSVersion.TLSv1_2
     def __init__(self):
         self.bot = None  # bot 实例引用
         self._started = False
         self.fastapi_url =  config.Fastapi_url
-        self.client = httpx.AsyncClient(timeout=60,verify=self.context)  # 创建异步HTTP客户端
+        self.client = httpx.AsyncClient(timeout=60)  # 创建异步HTTP客户端
 
     async def _ensure_started(self):
         """确保所有任务已启动"""
